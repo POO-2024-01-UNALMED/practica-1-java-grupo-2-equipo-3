96d51300c71ffc6e46b8c3e33bd45ea392c6a1d1
lladinog <159490210+lladinog@users.noreply.github.com>
2024-07-28 04:13:35 -0500

    eliminar clase cliente

--- gestorAplicacion/gestionHum/Cliente.java
+++ gestorAplicacion/gestionHum/Cliente.java
@@ -4,11 +4,5 @@ import java.util.ArrayList;
 
 import gestorAplicacion.manejoReserva.Actividad;
 
-public class Cliente extends Persona {
 
-    
-    public void cancelarActividad(Actividad actividad, ArrayList<Integer> fecha) {
-		//NO SE QUE HACEEEER
-		
-	}
-}
+
