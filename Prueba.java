d1cd3ad51e43a384c527504c9276663e2f495b63
origimed <allopezp@unal.edu.co>
2024-07-31 20:27:00 -0500

    getter y setter de clase persona

---
+++ Prueba.java
@@ -0,0 +1,65 @@
+import gestorAplicacion.*;
+import gestorAplicacion.manejoReserva.Reserva;
+import uiMain.*;
+import java.util.ArrayList;
+
+public class Prueba {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> fecha = new ArrayList<Integer>();
+
+        fecha.add(30);
+        fecha.add(12);
+        fecha.add(2020);
+
+        System.out.println(mostrarDias(5, fecha));
+
+        
+    }
+    
+
+
+
+    public static ArrayList<ArrayList<Integer>> mostrarDias(int cantidadDias, ArrayList<Integer> fecha) {
+        int dia = fecha.get(0);
+        int mes = fecha.get(1);
+        int año = fecha.get(2);
+
+        ArrayList<ArrayList<Integer>> listaFechas = new ArrayList<>();
+        int[] diasPorMes = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        while (cantidadDias > 0) {
+            int diasMes = diasPorMes[mes - 1];
+            int cantidadRestante = cantidadDias;
+            for (int i = 1; i < cantidadDias + 1; i++) {
+                if (dia > diasMes) {
+                    break;
+                }
+                ArrayList<Integer> fechas = new ArrayList<>();
+                fechas.add(dia);
+                fechas.add(mes);
+                fechas.add(año);
+                listaFechas.add(fechas);
+                dia++;
+                cantidadRestante--;
+            }
+
+            if (cantidadRestante == 0) {
+                break;
+            }
+
+            cantidadDias = cantidadRestante;
+            dia = 1;
+            mes++;
+            if (mes > 12) {
+                mes = 1;
+                año++;
+            }
+        }
+        return listaFechas;
+    }
+
+    
+
+}
+
--- gestorAplicacion/gestionHum/Persona.java
+++ gestorAplicacion/gestionHum/Persona.java
@@ -2,24 +2,76 @@ package gestorAplicacion.gestionHum;
 
 //Luego importo bien lo que falta
 import java.util.ArrayList;
-
-import gestorAplicacion.enums.Idiomas;
 import gestorAplicacion.manejoReserva.Destino;
 import gestorAplicacion.manejoReserva.Grupo;
 //Falta utilizar la accesibilidad
-
 public class Persona {
-	private String nombre;
-    private ArrayList<Idiomas> idiomas;
-    private Destino destino;
+    String nombre;
     int edad;
-    private ArrayList<Object> seguro;    //Array list de 4 puestos, debo leer como se escribía
+    Destino destino;
+    String[] idioma;
+    String[] seguro;    //Array list de 4 puestos, debo leer como se escribía
     Grupo grupo;
-    
-    public Persona(String nombre, int edad) {
-    	this.nombre = nombre;
-    	this.edad = edad;
+
+
+
+    public Persona(String nombre, int edad, Destino destino, String[] idioma, String[] seguro, Grupo grupo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.destino = destino;
+        this.idioma = idioma;
+        this.seguro = seguro;
+        this.grupo = grupo;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    } 
+
+    public void setDestino(Destino destino) {
+        this.destino = destino;
+    }
+
+    public void setIdioma(String[] idioma) {
+        this.idioma = idioma;
+    }
+
+    public void setSeguro(String[] seguro) {
+        this.seguro = seguro;
+    }
+
+    public void setGrupo(Grupo grupo) {
+        this.grupo = grupo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public Destino getDestino() {
+        return destino;
+    }
+
+    public String[] getIdioma() {
+        return idioma;
+    }
+
+    public String[] getSeguro() {
+        return seguro;
+    }
+
+    public Grupo getGrupo() {
+        return grupo;
+    }
+
+    
 
 }
